a6d08f48c4456b3605e1553ccb9c50294e30ce39
Natasa Mijatovic <66488937+tashomy@users.noreply.github.com>
2021-09-21 04:22:16 +0200

    Update index.js
    Text changed

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -3,12 +3,12 @@ var sum = 0;
 
 function addThree() {
   sum += 3;
-  console.log("sum from addThree:", sum);
+  console.log("addThree:", sum);
 }
 
 function addFive() {
   sum += 5;
-  console.log("sum from addFive:", sum);
+  console.log("addFive:", sum);
 }
 
 addThree();
